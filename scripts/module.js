e40186777df7269ea5d6232f0a4ed61229116c2b
Kevin <chankevi@oregonstate.edu>
2024-05-09 02:15:31 -0700

    Grave Matter module.js update
    SCVMBIRTHER module JS update for Guilt bearer, death hunter, and Rotting Necromancer.

--- scripts/module.js
+++ scripts/module.js
@@ -36,5 +36,11 @@ Hooks.once('init', async function() {
   	"Compendium.morkborg-3p.mork-borg-3p-items.g8JnRKWDxp92Bbs1",
   	// the bellfounder
   	"Compendium.morkborg-3p.mork-borg-3p-items.JNvP1NLKQ577RBQw",
+  	// rotting necromancer
+  	"Compendium.morkborg-3p.mork-borg-3p-items.PHhZQuw3dn8Ssfhe",
+  	// death hunter
+  	"Compendium.morkborg-3p.mork-borg-3p-items.yaprmub7NRMrmgzn",
+	// guilt bearer
+  	"Compendium.morkborg-3p.mork-borg-3p-items.kjvlgGzKGSF2cv4y",
   );
 });
